8ebed4640920f51fdaa91177f8a98e8e65601f76
NathanGLopes <nathangclopes@gmail.com>
2025-09-01 14:44:47 -0300

    Atividade 2 realizada.

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,1 +1,12 @@
-# Sua solução aqui
+a = int(input("Digite o valor do lado a: "))
+b = int(input("Digite o valor do lado b: "))
+c = int(input("Digite o valor do lado c: "))
+if a < b+c and b < a+c and c < a+b:
+    if a == b and b == c:
+        print("Triângulo Equilátero")
+    elif a == b or b == c or a == c:
+        print("Triângulo Isósceles")
+    elif a != b and b != c and a != c:
+        print("Triângulo Escaleno")
+else:
+    print("Não é um triângulo")
